43263c53c5156fe2e5c66ccfd642cd3e67004a08
ChrisSav713 <106366587+ChrisSav713@users.noreply.github.com>
2022-06-26 14:55:42 -0400

    done

---
+++ always_hungry.js
@@ -0,0 +1,20 @@
+function alwaysHungry(arr) {
+    eaten = false;
+    output = "";
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == "food") {
+            eaten = true;
+            output += "yummy "
+        }
+    }
+    if (eaten) {
+        console.log(output);
+    } else {
+        console.log("I'm hungry");
+    }
+}
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
---
+++ array_reverse.js
@@ -0,0 +1,11 @@
+function reverse(arr) {
+    for (i = 0; i < arr.length / 2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
